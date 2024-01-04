66dd3017c994ccef2a897740fd0f744fc2a5f528
aminuv <mohamed.elmarbouh07@gmail.com>
2024-01-04 20:59:29 +0000

    the last one

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,12 @@
 import Car from './10-car';
 
 export default class EVCar extends Car {
-  constructor(brand, motor, color, range) {
-    super(brand, motor, color);
-    this._range = range;
-  }
+	constructor(brand, motor, color, range) {
+		super(brand, motor, color);
+		this._range = range;
+	}
 
-  // git clone it from 10-Car
-  cloneCar() {
-    return new Car();
-  }
+	cloneCar() {
+		return new Car();
+	}
 }
